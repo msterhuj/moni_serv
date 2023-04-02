ab0539230b458e84d1dc2c5bc3fbb9b525e2eb29
msterhuj <gabin.lanore@gmail.com>
2023-04-02 17:11:42 +0200

    feat: admin dash

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('', views.dashboard_view, name='dashboard'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('agent_add', views.create_agent_view, name='create_agent')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,55 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.conf import settings
+from django.urls import reverse
 
-def login(request):
+from api.models import MoniAgent
+from dashboard.models import Page, PageMetricGroup, PageIncident
+
+def login_view(request):
+
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+
+    user = authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        login(request, user)
+        return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
     return render(request, 'auth/login.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
+
+def dashboard_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    
+    agents = MoniAgent.objects.all()
+
+
+    return render(request, 'admin/dashboard.html', {
+        "user": request.user,
+        "agents": agents
+    })
+
+def create_agent_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+    agent_name = request.POST.get("agent_name", None)
+    if agent_name:
+        agent = MoniAgent()
+        agent.name = agent_name
+        agent.token = "random"
+        agent.save()
+        return render(request, 'admin/add_agent.html', {
+            "create_ok": True,
+            "token": agent.token
+        })
+    else:
+        return render(request, 'admin/add_agent.html', {
+            "create_ok": False
+        })
